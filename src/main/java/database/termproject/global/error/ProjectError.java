ba5ce2f45c45d1642305f14a0e0f25714a977401
amm0124 <amm012450@gmail.com>
2024-12-01 18:03:11 +0900

    feat : error enum 등록 #12

--- src/main/java/database/termproject/global/error/ProjectError.java
+++ src/main/java/database/termproject/global/error/ProjectError.java
@@ -8,8 +8,16 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ProjectError {
 
+    // 회원
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원을 찾을 수 없습니다."),
-    EXIST_MEMBER(HttpStatus.CONFLICT , "이미 존재하는 회원입니다.")
+    EXIST_MEMBER(HttpStatus.CONFLICT , "이미 존재하는 회원입니다."),
+
+    //Posting
+    POSTING_NOT_FOUND(HttpStatus.NOT_FOUND, "찾을 수 없는 글입니다."),
+
+
+    //Comment
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "찾을 수 없는 댓글입니다.")
 
     ;
 
