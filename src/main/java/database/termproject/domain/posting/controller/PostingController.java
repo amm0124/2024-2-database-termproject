c5aaa4a8805437a479410ca88062d820c8a7dc07
amm0124 <amm012450@gmail.com>
2024-12-01 16:32:21 +0900

    feat : member 객체를 security context에서 받아오도록 구현 #12

--- src/main/java/database/termproject/domain/posting/controller/PostingController.java
+++ src/main/java/database/termproject/domain/posting/controller/PostingController.java
@@ -3,6 +3,7 @@ package database.termproject.domain.posting.controller;
 import database.termproject.domain.posting.dto.request.PostingRequest;
 import database.termproject.domain.posting.service.PostingServiceImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/posting")
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class PostingController {
 
     private final PostingServiceImpl postingService;
 
-    @Secured({"ROLE_USER"})
+    @Secured({"ROLE_USER", "ROLE_ANONYMOUS"})
     @PostMapping("/free")
     public ResponseEntity<?> createFreePosting(PostingRequest postingRequest) {
-        //TODO : 세션에서 member 객체로 변환해야 함
         postingService.createFreePosting(postingRequest);
         return ResponseEntity.ok().build();
     }
--- src/main/java/database/termproject/domain/posting/service/PostingServiceImpl.java
+++ src/main/java/database/termproject/domain/posting/service/PostingServiceImpl.java
@@ -6,13 +6,18 @@ import database.termproject.domain.posting.repository.PostingJPARepository;
 import database.termproject.domain.posting.dto.request.PostingRequest;
 import database.termproject.domain.posting.dto.response.PostingResponse;
 import database.termproject.domain.posting.entity.PostingType;
+import database.termproject.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PostingServiceImpl implements PostingService<PostingRequest> {
 
     private final PostingJPARepository postingJPARepository;
@@ -23,8 +28,9 @@ public class PostingServiceImpl implements PostingService<PostingRequest> {
         String content = postingRequest.content();
         PostingType postingType = PostingType.FREE;
 
-        //TODO : Member 객체 spring context에서 가져와야 함
-        Member member = Member.builder().build();
+        Member member = getMember();
+
+        log.info("member :  " + member.getEmail());
 
         Posting posting = Posting
                 .builder()
@@ -38,6 +44,14 @@ public class PostingServiceImpl implements PostingService<PostingRequest> {
         return PostingResponse.fromEntity(posting);
     }
 
+
+    private Member getMember(){
+        Authentication authentication = SecurityContextHolder
+                .getContext().getAuthentication();
+        Object impl = authentication.getPrincipal();
+        Member member = ((UserDetailsImpl) impl).getMember();
+        return member;
+    }
 
     /*public List<PostingResponse> getPosting(){
         //TODO : jwt에서 가져와야 함
