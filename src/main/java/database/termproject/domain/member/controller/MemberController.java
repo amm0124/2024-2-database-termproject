e41bcf224e4c30774f90f6ef5ed407ba57f4ea07
amm0124 <amm012450@gmail.com>
2024-12-03 13:16:56 +0900

    feat : 멤버 CRUD 컨트롤러#23

--- src/main/java/database/termproject/domain/member/controller/MemberController.java
+++ src/main/java/database/termproject/domain/member/controller/MemberController.java
@@ -1,7 +1,10 @@
 package database.termproject.domain.member.controller;
 
 
+import database.termproject.domain.member.dto.request.MemberEditRequest;
+import database.termproject.domain.member.dto.request.MemberEmailVerifyRequest;
 import database.termproject.domain.member.dto.request.MemberSignUpRequestDto;
+import database.termproject.domain.member.dto.response.MemberResponse;
 import database.termproject.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +19,10 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping
-    @Secured("{ROLE_ADMIN, ROLE_MANAGER, ROLE_USER, ROLE_ANONYMOUS}")
-    public String getMemberInfo() {
-        return "HI";
+    @Secured({"ROLE_USER", "ROLE_ADMIN", "ROLE_MANAGER", "ROLE_ANONYMOUS"})
+    public ResponseEntity<?> getMemberInfo() {
+        return ResponseEntity.ok(memberService.
+                getMemberInfo());
     }
 
     @PostMapping("/signup")
@@ -27,14 +31,42 @@ public class MemberController {
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/email-verify")
-    public ResponseEntity<?> verifyByEmail(){
-        //TODO : 이메일 인증
+    @PostMapping("/try-verify")
+    @Secured({"ROLE_ANONYMOUS"})
+    public ResponseEntity<?> tryVerify(){
+        memberService.generateAuthCode();
         return ResponseEntity.ok().build();
     }
 
+    @PostMapping("/email-code-verify")
+    @Secured({"ROLE_ANONYMOUS"})
+    public ResponseEntity<?> verifyByEmail(@RequestBody MemberEmailVerifyRequest memberEmailVerifyRequest){
 
+        return ResponseEntity.ok().build();
+    }
 
+    @PutMapping("/edit")
+    @Secured({"ROLE_USER", "ROLE_ADMIN", "ROLE_MANAGER", "ROLE_ANONYMOUS"})
+    public ResponseEntity<?> editMember(@RequestBody MemberEditRequest memberEditRequest){
+        return ResponseEntity.ok(
+                memberService.editMemberInfo(memberEditRequest)
+        );
+    }
+
+    @DeleteMapping("/remove")
+    @Secured({"ROLE_USER", "ROLE_ADMIN", "ROLE_MANAGER", "ROLE_ANONYMOUS"})
+    public ResponseEntity<?> removeMember(){
+        return ResponseEntity.ok(
+                memberService.softDeleteMember()
+        );
+    }
+
+    //TODO : 복구 로직 작성해야 함
+    @PostMapping("/restore")
+    @Secured({"ROLE_ADMIN"})
+    public ResponseEntity<?> restoreMember(){
+        return ResponseEntity.ok().build();
+    }
 
 }
 
