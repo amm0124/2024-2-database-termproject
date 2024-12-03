1c24a9cdd16564c68b40780ae32b3a2ba07ca44e
amm0124 <amm012450@gmail.com>
2024-12-03 13:18:05 +0900

    feat : 멤버 메서드 추가/프로필 추가 #23

--- src/main/java/database/termproject/domain/member/entity/MemberProfile.java
+++ src/main/java/database/termproject/domain/member/entity/MemberProfile.java
@@ -1,17 +1,18 @@
 package database.termproject.domain.member.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberProfile {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @OneToOne
-    private Member member;
 
     private String name;
 
@@ -21,5 +22,31 @@ public class MemberProfile {
 
     private String phoneNumber;
 
+    @Builder
+    public MemberProfile(String name, String address, String addressDetail, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.addressDetail = addressDetail;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public MemberProfile editMemberProfile(String name, String address, String addressDetail, String phoneNumber){
+        if (name != null) {
+            this.setName(name);
+        }
+
+        if (address != null) {
+            this.setAddress(address);
+        }
+
+        if (addressDetail != null) {
+            this.setAddressDetail(addressDetail);
+        }
+
+        if (phoneNumber != null) {
+            this.setPhoneNumber(phoneNumber);
+        }
+        return this;
+    }
 
 }
--- src/main/java/database/termproject/domain/member/repository/MemberRepository.java
+++ src/main/java/database/termproject/domain/member/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package database.termproject.domain.member.repository;
 
 import database.termproject.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +11,11 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+    @Query("SELECT m FROM Member m JOIN FETCH m.memberProfile WHERE m.id = :id")
+    Optional<Member> findByIdWithProfile(@Param("id") Long id);
+
+    @Query("SELECT m FROM Member m LEFT JOIN FETCH m.memberProfile WHERE m.email = :email")
+    Optional<Member> findByEmailWithProfile(@Param("email") String email);
+
+
 }
