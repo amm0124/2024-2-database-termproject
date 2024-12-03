a4404c5c3b024d1bafd12e99964c788d920795f5
amm0124 <amm012450@gmail.com>
2024-12-03 15:23:29 +0900

    chore : 댓글, 대댓글 api 분리로 인한 request dto 변경 + repository naming 변경 #25

--- src/main/java/database/termproject/domain/posting/_comment/CommentRepository.java
+++ src/main/java/database/termproject/domain/posting/_comment/CommentRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByPostingId(Long postingId);
-    List<Comment> findByPostingIdAndIsDeletedFalseOrderByDepthAsc(Long postingId);
+    List<Comment> findByPosting_IdOrderByDepth(Long postingId);
+
+    Optional<Comment> findByPosting_IdAndParentComment_Id(Long postingId, Long parentCommentId);
 }
