5caf192f140aa07750b47532f3adefdff6762b8f
amm0124 <amm012450@gmail.com>
2024-12-03 13:18:35 +0900

    feat : 회원가입 dto 변경 + 멤버 crud service 작성 #23

--- src/main/java/database/termproject/domain/member/dto/request/MemberSignUpRequestDto.java
+++ src/main/java/database/termproject/domain/member/dto/request/MemberSignUpRequestDto.java
@@ -2,6 +2,10 @@ package database.termproject.domain.member.dto.request;
 
 public record MemberSignUpRequestDto(
         String email,
-        String password
+        String password,
+        String name,
+        String address,
+        String addressDetail,
+        String phoneNumber
 ) {
 }
