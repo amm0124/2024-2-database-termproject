85a0c69d68ebe1b703f82333a98bb54f497f675c
amm0124 <amm012450@gmail.com>
2024-12-04 17:30:16 +0900

    feat : entity fetch join #38

--- src/main/java/database/termproject/domain/matchingjoin/entity/MatchingJoin.java
+++ src/main/java/database/termproject/domain/matchingjoin/entity/MatchingJoin.java
@@ -5,6 +5,7 @@ import database.termproject.domain.member.entity.Member;
 import database.termproject.domain.posting.entity.Matching;
 import database.termproject.global.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -20,14 +21,28 @@ import lombok.NoArgsConstructor;
 public class MatchingJoin extends BaseEntity {
 
     @ManyToOne
+    @JoinColumn(name = "member_id")
     private Member member;
 
     @ManyToOne
+    @JoinColumn(name = "matching_id")
     private Matching matching;
 
+    private Integer count;
+
     @Builder
-    public MatchingJoin(Member member, Matching matching) {
+    public MatchingJoin(Member member, Matching matching, Integer count) {
         this.member = member;
         this.matching = matching;
+        this.count = count;
     }
+
+    public Long getMemberId(){
+        return member.getId();
+    }
+
+    public String getMemberName(){
+        return member.getMemberProfile().getName();
+    }
+
 }
