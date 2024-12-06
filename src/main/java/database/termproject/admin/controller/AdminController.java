b36b483cc8c68eae7f27bc8848f46bfa8e844589
amm0124 <amm012450@gmail.com>
2024-12-06 21:02:12 +0900

    feat : 관리자 콘솔 페이지 controller, restcontroller, service 작성 #43

---
+++ src/main/java/database/termproject/admin/controller/AdminController.java
@@ -0,0 +1,33 @@
+package database.termproject.admin.controller;
+
+
+import database.termproject.admin.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping ("/admin")
+public class AdminController {
+
+
+    @GetMapping("/login")
+    public String login() {
+        return "loginPage";
+    }
+
+
+    //secured
+    @GetMapping("/console")
+    public String adminConsole() {
+        return "admin-console";
+    }
+
+}
